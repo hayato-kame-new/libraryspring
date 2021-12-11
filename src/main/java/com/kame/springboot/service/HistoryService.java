aa8074a578f6e108a708068dcee063fc5d768ec3
S Kame <skame@kameyamamisanoiMac.local>
2021-12-11 18:27:15 +0900

    -f

--- src/main/java/com/kame/springboot/service/HistoryService.java
+++ src/main/java/com/kame/springboot/service/HistoryService.java
@@ -3,14 +3,8 @@ package com.kame.springboot.service;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import com.kame.springboot.entity.History;
-import com.kame.springboot.repository.HistoryRepository;
 
 @Service
 @Transactional  // クラスに対して記述した設定はメソッドで記述された設定で上書きされる このクラスで @Transactionalをつけて、コントローラにはつけない
@@ -19,8 +13,7 @@ public class HistoryService {
 	// historiesテーブルは booksテーブルとリレーションがある historiesテーブルが子テーブル  booksテーブルが親テーブル
 	// historiesテーブルは membersテーブルとリレーションがある historiesテーブルが子テーブル  membersテーブルが親テーブル
 
-		@Autowired
-		HistoryRepository historyRepository;
+		
 		
 		// @PersistenceContextは一つしかつけれない コントローラなどの方につけてたら削除する
 		@PersistenceContext // EntityManagerのBeanを自動的に割り当てるためのもの サービスクラスにEntityManagerを用意して使う。その他の場所には書けません。１箇所だけ
@@ -33,10 +26,7 @@ public class HistoryService {
 		 * @param pageable
 		 * @return Page<History>
 		 */
-		public Page<History> getAllHistories(Pageable pageable) {
-			
-	         return historyRepository.findAll(pageable);		
-	    }
+		
 	
 
 }
